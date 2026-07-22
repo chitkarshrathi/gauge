bca50194f6998a128d7a4d762d4aecdfb7531e55
Chitkarsh Rathi <chitkarsh.rathi@gmail.com>
2026-07-21 23:10:53 -0600

    Initial Commit

---
+++ Gauge/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Gauge
+//
+//  Created by Chitkarsh Rathi on 7/21/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
